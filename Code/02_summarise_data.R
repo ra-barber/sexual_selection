19f41cac9d626bd69a4b9fde1b8d3756bda52bc6
Rob Barber <r.barber19@imperialac.uk>
2023-08-07 11:51:18 +0100

    Added VIF value check. This script needs cleaning.

--- Code/02_summarise_data.R
+++ Code/02_summarise_data.R
@@ -469,8 +469,8 @@ sex_meanplot <- function(predictor){
               sex_se = sd(sexual_score)/sqrt(length(sexual_score)))
   
   ggplot(grouped_data, aes(x = trait, y = sex_mean)) +
-    geom_errorbar(aes(ymin = sex_mean - sex_se, ymax = sex_mean + sex_se), width =0.5) + 
-    geom_point(size =2) + ylab("Sexual selection") + xlab(predictor) + theme_classic()
+    geom_errorbar(aes(ymin = sex_mean - sex_se, ymax = sex_mean + sex_se),  width = 0.2, linewidth = 0.5) + 
+    geom_point(size =3) + ylab("Sexual selection") + xlab(predictor) + theme_classic()
 }
 
 
@@ -500,71 +500,47 @@ ggcorr(lat_variables, label = TRUE, method =c("everything","spearman"), label_ro
 ###############################################################################
                        #### Check  VIF values ####
 
-# # Uncentered.
-# model_formula <- "sexual_score ~ territory_binary + migration_binary + 
-#     trophic_binary + chick_z + trophic_binary*temp_seasonality_z + 
-#     trophic_binary*territory_binary + trophic_binary*npp_z + 
-#     chick_z*temp_seasonality_z + chick_z*territory_binary + chick_z*npp_z"
-# 
-# # Centered.
-# model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
-#     trophic_level_c + chick_sqrt_z + trophic_level_c*temp_seasonality_z + 
-#     trophic_level_c*terr_bi_c + trophic_level_c*npp_z + 
-#     chick_sqrt_z*temp_seasonality_z + chick_sqrt_z*terr_bi_c + chick_sqrt_z*npp_z"
-# 
-# # No NPP
-# model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
-#     trophic_level_c + chick_z + trophic_level_c*temp_seasonality_z + 
-#     trophic_level_c*terr_bi_c + 
-#     chick_z*temp_seasonality_z + chick_z*terr_bi_c"
-# 
-# # No NPP centerered.
-# model_formula <- "sexual_score ~ territory_binary + migration_binary + 
-#     trophic_binary + chick_z + trophic_binary*temp_seasonality_z + 
-#     trophic_binary*territory_binary + 
-#     chick_z*temp_seasonality_z + chick_z*territory_binary"
-# 
-# # With chick sqrt.
-# model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
-#     trophic_level_c + trophic_level_c*temp_seasonality_z  + 
-#     chick_sqrt_z*temp_seasonality_z"
-# 
-# # Simple.
-# model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
-#     trophic_level_c + chick_z + trophic_level_c*temp_seasonality_z"
-# 
-# 
-# 
-# model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
-#     trophic_level_c + chick_sqrt_z + trophic_level_c*temp_seasonality_z + 
-#     trophic_level_c*terr_bi_c + trophic_level_c*npp_z + 
-#     chick_sqrt_z*temp_seasonality_z + chick_sqrt_z*terr_bi_c + chick_sqrt_z*npp_z"
-
 
 # Model formula for VIF values.
 model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
     trophic_level_c + trophic_level_c*temp_seasonality_z + 
     trophic_level_c*terr_bi_c"
 
+model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
+    trophic_level_c + trophic_level_c*temp_seasonality_z + 
+    terr_bi_c*temp_seasonality_z"
+
+model_formula <- "sexual_score ~ terr_bi_c + migration_bi_c + 
+    trophic_level_c + trophic_level_c*temp_seasonality_z + 
+    terr_bi_c*temp_seasonality_z + migration_bi_c*temp_seasonality_z"
+
 
 library(car)
+
+# Check full model VIF.
 test_model <- lm(model_formula, data = model_data)
-
 vif(test_model) %>% round(digits = 2)
-vif(test_model, type = "predictor")
-sqrt(3)
 
-
+# High certainty VIF.
 test_model <- lm(model_formula, data = high_data)
-
 vif(test_model) %>% round(digits = 2)
 
-summary(test_model)
 
-model_data$tree_tip <- gsub(" ", "_", model_data$birdtree_name)
-row.names(model_data) <- model_data$tree_tip
+# See all two-way interactions.
+model_formula <- "sexual_score ~ (terr_bi_c + migration_bi_c + 
+    trophic_level_c + temp_seasonality_z)^2"
+test_model <- lm(model_formula, data = model_data)
+vif(test_model) %>% round(digits = 2)
 
-phylolm(model_formula, model_data, tree) %>% summary()
+
+
+
+# summary(test_model)
+# 
+# model_data$tree_tip <- gsub(" ", "_", model_data$birdtree_name)
+# row.names(model_data) <- model_data$tree_tip
+# 
+# phylolm(model_formula, model_data, tree) %>% summary()
 
 
 ###############################################################################
